71983ba059cfad521aff38cb2de069aca8a2ca97
kimsungwon1 <kimsunguen@gmail.com>
2025-08-25 14:25:03 +0900

    m

--- sources/mainwindow.cpp
+++ sources/mainwindow.cpp
@@ -6,6 +6,22 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    socket = new QTcpSocket(this);
+    socket->connectToHost("192.168.2.29", 8085);
+
+    if (!socket->waitForConnected(3000)) {  // 최대 3초 대기
+        qDebug() << "서버 접속 실패:" << socket->errorString();
+        return;
+    }
+    qDebug() << "서버 접속 성공";
+
+    QObject::connect(&socket, &QTcpSocket::readyRead, readFromServer);
+
+    QObject::connect(&socket, &QTcpSocket::disconnected, [&]() {
+        qDebug() << "서버 연결 끊김";
+        // this->deleteLater();
+    });
 }
 
 MainWindow::~MainWindow()
